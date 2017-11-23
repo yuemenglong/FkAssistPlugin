c3cb71cdc69db169c8af53e6419a740435cc475f
yuemenglong <yuemenglong@126.com>
2017-11-23 14:32:26 +0800

    add: 开始解决按键无响应问题

--- ClassLibrary/FkPlugin.cs
+++ ClassLibrary/FkPlugin.cs
@@ -12,9 +12,12 @@ namespace ClassLibrary
     public class FkPlugin : IEnhancedPlugin
     {
         HashSet<String> _set = new HashSet<string>();
+        GameObject _go = new GameObject();
+        private ObjMoveRotAssistMgr _assist;
 
         public void OnApplicationStart()
         {
+            _assist = _go.AddComponent<ObjMoveRotAssistMgr>();
             Logger.Log("YML-OnApplicationStart");
         }
 
@@ -35,73 +38,9 @@ namespace ClassLibrary
 
         public void OnUpdate()
         {
-            return;
-            if (Input.GetKeyDown(KeyCode.V))
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                Logger.Log("*************************************************************");
-                var objs = Object.FindObjectsOfType<GameObject>();
-                Logger.Log("Object Count: " + objs.Length);
-                var localSet = new HashSet<String>();
-                foreach (var obj in objs)
-                {
-                    localSet.Add(Kit.GetGameObjectPathAndPos(obj));
-                }
-                foreach (var s in _set)
-                {
-                    if (!localSet.Contains(s))
-                    {
-                        Logger.Log("- " + s);
-                    }
-                }
-                Logger.Log("==============================================================");
-                foreach (var s in localSet)
-                {
-                    if (!_set.Contains(s))
-                    {
-                        Logger.Log("+ " + s);
-                    }
-                }
-                _set = localSet;
-            }
-            if (Input.GetMouseButtonDown(1))
-            {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    Logger.Log(Kit.GetGameObjectPathAndPos(hit.transform.gameObject));
-                    Logger.Log(hit.transform.tag, hit.transform.gameObject.GetType().FullName);
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-//                var go = GameObject.Find("/StudioScene/GuideObjectWorkplace/M Root(Clone)/rotation");
-//                if (go != null)
-//                {
-//                    Logger.Log(go.GetType().FullName);
-//                }
-//                var gos = GameObject.FindObjectsOfType<GameObject>();
-//                foreach (var go in gos)
-//                {
-//                    if (go.name == "rotation")
-//                    {
-//                        Logger.Log(Kit.GetGameObjectPathAndPos(go));
-//                        go.transform.Rotate(10,10,10);
-//                    }
-//                }
-                var cs = GameObject.Find("CommonSpace");
-                Logger.Log("" + (cs == null));
-                if (cs != null)
-                {
-                    foreach (var child in Kit.LoopChildren(cs))
-                    {
-                        Logger.Log(Kit.GetGameObjectPathAndPos(child));
-                        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        var pos = Camera.main.WorldToScreenPoint(child.transform.position);
-                        Logger.Log(Kit.VecStr(pos));
-                        sphere.transform.position = child.transform.position;
-                    }
-                }
+                Logger.Log("Plugin A", _assist.enabled + "");
             }
         }
 
--- ClassLibrary/Kit.cs
+++ ClassLibrary/Kit.cs
@@ -43,10 +43,81 @@ namespace ClassLibrary
             }
             return list.ToArray();
         }
-        
+
         public static String VecStr(Vector3 v)
         {
             return String.Format("[{0},{1},{2}]", v.x, v.y, v.z);
+        }
+
+        public static void backup()
+        {
+//            if (Input.GetKeyDown(KeyCode.V))
+//            {
+//                Logger.Log("*************************************************************");
+//                var objs = Object.FindObjectsOfType<GameObject>();
+//                Logger.Log("Object Count: " + objs.Length);
+//                var localSet = new HashSet<String>();
+//                foreach (var obj in objs)
+//                {
+//                    localSet.Add(Kit.GetGameObjectPathAndPos(obj));
+//                }
+//                foreach (var s in _set)
+//                {
+//                    if (!localSet.Contains(s))
+//                    {
+//                        Logger.Log("- " + s);
+//                    }
+//                }
+//                Logger.Log("==============================================================");
+//                foreach (var s in localSet)
+//                {
+//                    if (!_set.Contains(s))
+//                    {
+//                        Logger.Log("+ " + s);
+//                    }
+//                }
+//                _set = localSet;
+//            }
+//            if (Input.GetMouseButtonDown(1))
+//            {
+//                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//                RaycastHit hit;
+//                if (Physics.Raycast(ray, out hit))
+//                {
+//                    Logger.Log(Kit.GetGameObjectPathAndPos(hit.transform.gameObject));
+//                    Logger.Log(hit.transform.tag, hit.transform.gameObject.GetType().FullName);
+//                }
+//            }
+//            if (Input.GetKeyDown(KeyCode.X))
+//            {
+//                var go = GameObject.Find("/StudioScene/GuideObjectWorkplace/M Root(Clone)/rotation");
+//                if (go != null)
+//                {
+//                    Logger.Log(go.GetType().FullName);
+//                }
+//                var gos = GameObject.FindObjectsOfType<GameObject>();
+//                foreach (var go in gos)
+//                {
+//                    if (go.name == "rotation")
+//                    {
+//                        Logger.Log(Kit.GetGameObjectPathAndPos(go));
+//                        go.transform.Rotate(10,10,10);
+//                    }
+//                }
+//                var cs = GameObject.Find("CommonSpace");
+//                Logger.Log("" + (cs == null));
+//                if (cs != null)
+//                {
+//                    foreach (var child in Kit.LoopChildren(cs))
+//                    {
+//                        Logger.Log(Kit.GetGameObjectPathAndPos(child));
+//                        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+//                        var pos = Camera.main.WorldToScreenPoint(child.transform.position);
+//                        Logger.Log(Kit.VecStr(pos));
+//                        sphere.transform.position = child.transform.position;
+//                    }
+//                }
+//            }
         }
     }
 }
